4ff962192b775a12b3691b23ce90be798057372c
Sandro Wong <saloiofun@gmail.com>
2017-11-05 17:04:48 -0800

    fix active navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () =>
   <nav className='navbar navbar-expand-lg navbar-dark bg-dark rounded mb-4'>
@@ -8,8 +9,24 @@ const Navbar = () =>
     </button>
     <div className='collapse navbar-collapse' id='navbarNav'>
       <div className='navbar-nav ml-auto'>
-        <a className='nav-item nav-link active' href='/search'>Search</a>
-        <a className='nav-item nav-link' href='/saved'>Saved Articles</a>
+        <a
+          className={
+            window.location.pathname === '/' ||
+            window.location.pathname === '/search'
+              ? 'nav-item nav-link active'
+              : 'nav-item nav-link'
+        }
+          href='/search'
+        >Search
+        </a>
+        <a className={
+            window.location.pathname === '/saved'
+              ? 'nav-item nav-link active'
+              : 'nav-item nav-link'
+        }
+          href='/saved'
+        >Saved Articles
+        </a>
       </div>
     </div>
   </nav>

5bbed42bd775aa9d934901c929f2cbd533df45a5
Sandro Wong <saloiofun@gmail.com>
2017-11-05 21:59:04 -0800

    add footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
-const Footer = props => <footer {...props} />
+const Footer = props =>
+  <footer>
+    <hr />
+    <p className='text-right'><a href='https://github.com/saloiofun/react-nyt' target='_blank'>Proudly built using React.js</a></p>
+  </footer>
 
 export default Footer
